573c563f0405b9d0f22a064221760608d372b00d
BertrandHustle <scott.greenberg926@gmail.com>
2016-06-17 09:20:29 -0400

    commit BEFORE adding doug's fork of spotify API

--- src/main/java/com/example/EatBeatsController.java
+++ src/main/java/com/example/EatBeatsController.java
@@ -1,17 +1,17 @@
 package com.example;
 
+
 import com.wrapper.spotify.Api;
 import com.wrapper.spotify.exceptions.WebApiException;
-import com.wrapper.spotify.methods.AlbumSearchRequest;
+import com.wrapper.spotify.methods.RecommendationsRequest;
 import com.wrapper.spotify.methods.authentication.ClientCredentialsGrantRequest;
-import com.wrapper.spotify.models.Page;
-import com.wrapper.spotify.models.SimpleAlbum;
+import com.wrapper.spotify.models.ClientCredentials;
+import com.wrapper.spotify.models.Track;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
@@ -101,29 +101,41 @@ public class EatBeatsController {
     }
 
 
+    //creates random Spotify playlist based on seed tracks
+
+    //implimentation done by Doug Hughes
+
     @RequestMapping(path = "/create-playlist", method = RequestMethod.GET)
     public String createRecipe(HttpSession session) throws IOException, WebApiException {
 
-        final Api api = Api.DEFAULT_API;
+        //builds api object from application id/secret
+        final Api api = Api.builder()
+                        .clientId("f5b8721c375a43eb801334c0d4329a0d")
+                        .clientSecret("e4cf678de40843279f667da0b7dfabae").build();
 
-        //temp access token for playlists
-        final ClientCredentialsGrantRequest clientRequest = api.clientCredentialsGrant().grantType("client_credentials")
-                .basicAuthorizationHeader("5855c3febfa54cc4ab3558dca0a37def", "5a0942a8fe0d48bdb6ff5a984fbb5ca9").build();
+        /* Create a request object. */
+                final ClientCredentialsGrantRequest clientCredentialsGrantRequest = api.clientCredentialsGrant().build();
 
-        String test = clientRequest.get().getAccessToken();
-        System.out.println(test);
+        /* Use the request object to make the request, either asynchronously (getAsync) or synchronously (get) */
+                ClientCredentials clientCredentials = clientCredentialsGrantRequest.get();
 
-        RestTemplate restTemplate = new RestTemplate();
+        /* Set access token on the Api object so that it's used going forward */
+                api.setAccessToken(clientCredentials.getAccessToken());
+                final RecommendationsRequest recommendationsRequest = api.getRecommendations()
+                        .seedTrack("0CU30zif0WR5h1DI9oGtAF")
+                        .build();
 
-        final AlbumSearchRequest request = api.searchAlbums("Oscar Peterson Trio").offset(0).limit(3).build();
+                //try/catch block for print test
+                try {
+                    List<Track> tracks = recommendationsRequest.get();
+                    System.out.println("I got " + tracks.size() + " results!");
+                } catch (Exception e) {
+                    System.out.println("Something went wrong!" + e.getMessage());
+                }
+                int x = 1;
+                return "";
+            }
 
-        final Page<SimpleAlbum> albumSearchResult = request.get();
-
-        int x = 0;
-
-        return "";
-
-    }
 
     //login route
     @RequestMapping(path = "/login", method = RequestMethod.POST)
